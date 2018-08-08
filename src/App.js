ce4fce0d2d3cfbee1ccbc150a4192d6f244e69f6
Julio C Diaz <julio@vimbly.com>
2018-08-08 07:41:39 -0500

    - Eject command

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from'./App.css';
 import Person from './Person/Person';
 import Radium, {StyleRoot} from 'radium'
 
@@ -54,20 +54,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
 
     let persons = null;
+    let btnClass = '';
     if(this.state.showPersons){
       persons = (
         <div>
@@ -82,29 +71,25 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
       <StyleRoot>
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working</p>
+          <p className={assignedClasses.join(' ')}>This is really working</p>
           {/* working but waste resources */}
           {/* <button style={style} onClick={() => this.switchNameHandler('Yeral')}>Switch name</button> */}
-          <button style={style} onClick={this.togglePersonHandler}>Switch name</button>
+          <button className={btnClass} onClick={this.togglePersonHandler}>Switch name</button>
           {persons}
         </div>
       </StyleRoot>
