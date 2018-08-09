e2291088059dd458e1c040b09eb50489c40e88f4
Julio C Diaz <julio@vimbly.com>
2018-08-08 22:15:38 -0500

    - Refactor project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import classes from'./App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
 import Radium, {StyleRoot} from 'radium'
+import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
   state = {
@@ -56,40 +56,23 @@ class App extends Component {
   render() {
 
     let persons = null;
-    let btnClass = '';
     if(this.state.showPersons){
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-            click={this.deletePersonHandler.bind(this, index)}
-            name={person.name} 
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.changeNameHandler(event, person.id)} />
-          })}
-        </div>
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.changeNameHandler} />
       );
-
-      btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if(this.state.persons.length <= 2){
-      assignedClasses.push(classes.red);
-    }
-    if(this.state.persons.length <= 1){
-      assignedClasses.push(classes.bold);
     }
 
     return (
       <StyleRoot>
         <div className={classes.App}>
-          <h1>Hi, I'm a React App</h1>
-          <p className={assignedClasses.join(' ')}>This is really working</p>
-          {/* working but waste resources */}
-          {/* <button style={style} onClick={() => this.switchNameHandler('Yeral')}>Switch name</button> */}
-          <button className={btnClass} onClick={this.togglePersonHandler}>Switch name</button>
+          <Cockpit
+            appTitle={this.props.title}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonHandler} />
           {persons}
         </div>
       </StyleRoot>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '/Users/juliocesardiaz/Work/Practice/reactjs/react-complete-guide/src/containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App title="Relevant Persons" />, document.getElementById('root'));
 registerServiceWorker();
